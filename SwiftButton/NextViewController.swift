d4c14327baab0e3f19eb9dff6174e7b4a5a8d27b
modeverv <modeverv@gmail.com>
2019-02-06 02:19:36 +0900

    2019-02-06 02:19:36

---
+++ SwiftButton/NextViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NextViewController.swift
+//  SwiftButton
+//
+//  Created by seijiro on 2019/01/29.
+//  Copyright © 2019 norainu. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+  @IBOutlet var changeLabel: UILabel!
+  
+  @IBAction func change(_ sender: Any) {
+  }
+  /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SwiftButton/ViewController.swift
+++ SwiftButton/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var count = 5
+  var moji = "アイウエオ"
+
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    count = 10
+    print(count)
+
   }
 
 
